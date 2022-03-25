1e049264998912bde08bd21fa0042a5232a71b5e
madChill <nguyenhuycong.dev@gmail.com>
2022-03-26 00:23:29 +0700

    add way to build page

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index2.js',
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            }
+        ]
+    },
     plugins: [
         // new BundleAnalyzerPlugin()
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
       ],
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
 };
